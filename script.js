8c6cc434df30f28578ce908cb2b6dd6c1e2f1378
sAmMY-1320 <cuccaresesamuele@gmail.com>
2025-08-14 16:58:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function openCity(city) {
+    if (city === 'londra') {
+        window.location.href = 'https://www.visitlondon.com/';
+    } else if (city === 'newyork') {
+        window.location.href = 'https://www.nycgo.com/';
+    } else if (city === 'parigi') {
+        window.location.href = 'https://en.parisinfo.com/';
+    }
+}
